c6588c3468b0b69dbc24cf4fc798e1cd881a49b9
刘川 <liuchuan@liuchuandeMacBook-Pro.local>
2020-05-06 10:12:05 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
+<!--
+ * @Author: liuchuan
+ * @Date: 2020-05-06 10:08:19
+ * @LastEditTime: 2020-05-06 10:11:51
+ * @Description: 
+ -->
 <template>
   <div id="app">
     <router-link to="/test">test</router-link>
     <router-link to="/action">action</router-link>
-    <router-view/>
+    <p>git merge test</p>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
